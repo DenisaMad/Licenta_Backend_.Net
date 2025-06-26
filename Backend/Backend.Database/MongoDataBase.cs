c0269d6b3d6c781d370d9de99c7d956f1092f8a8
Denisa Madularea <140159933+DenisaMad@users.noreply.github.com>
2025-06-26 19:42:48 +0300

    Conexiune database si arhitectura proiectului

---
+++ Backend/Backend.Database/MongoDataBase.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Domain.User;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Database
+{
+    public class MongoDataBase
+    {
+        private readonly string Connection = "mongodb+srv://Admin:<23GMxBrkQnf9vta>@cluster0.bpvikfn.mongodb.net/";
+        public string Database = "LicentaDB";
+        public string UsersCollection = "Users";
+
+        public IMongoCollection<User> GetUserCollection() { 
+            IMongoClient client = new MongoClient(Connection);
+            IMongoDatabase database = client.GetDatabase(Database);
+            IMongoCollection<User> collection = database.GetCollection<User>(UsersCollection);
+            return collection;
+        }
+    }
+}
---
+++ Backend/Backend/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using Backend.Database;
+using Backend.Domain.User;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet(Name = "Users")]
+        public string GetUsers()
+        {
+            return "Buna ziua";
+            
+        }
+
+        [HttpPost(Name = "CreateUser")]
+        public async Task<string> PostUsers()
+        {
+            Console.WriteLine("enter name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter email");
+            string email = Console.ReadLine();
+            Console.WriteLine("enter password");
+            string password = Console.ReadLine();
+            Console.WriteLine("enter Number");
+            string PhoneNum = Console.ReadLine();
+            Console.WriteLine("enter age");
+            int age = int.Parse(Console.ReadLine());
+
+            MongoDataBase mongoDataBase = new MongoDataBase();
+            var collection = mongoDataBase.GetUserCollection();
+            User user = new User { Email = email, Name =name, Password=password, PhoneNumber=PhoneNum, age=age};
+            await collection.InsertOneAsync(user);
+            return "User creat cu succes!";
+        }
+
+        [HttpPut(Name = "UpdateUser")]
+        public string PutUsers()
+        {
+            return "User actualizat cu succes!";
+        }
+
+        [HttpDelete(Name = "DeleteUser")]
+        public string DeleteUsers()
+        {
+            return "User sters cu succes!";
+        }
+    }
+
+    
+}
